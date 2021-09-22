57e992a6c1863e48ef1de9865f1c49864daa8738
Nicholas Chin <njc94@scarletmail.rutgers.edu>
2021-09-22 18:29:14 -0400

    comment body in request using spread and set userId to session loggedinUserId

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,14 +1,15 @@
-const router = require('express').Router();
-const { Comment } = require('../../models/');
-const withAuth = require('../../utils/auth');
+const router = require("express").Router();
+const { Comment } = require("../../models/");
+const withAuth = require("../../utils/auth");
 
 // URL: /api/comment
-router.post('/', withAuth, async (req, res) => {
+router.post("/", withAuth, async (req, res) => {
   try {
     const newComment = await Comment.create({
       // TODO: COMMENT BODY IN REQUEST USING SPREAD
-
+      ...req.body,
       // TODO: SET USERID userId TO SESSION LOGGEDIN USERID
+      userId: req.session.userId,
     });
     res.json(newComment);
   } catch (err) {
